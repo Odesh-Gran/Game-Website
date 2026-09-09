e250d07af767deb8f3a2369ca69fe7bffed144ce
Odesh-Gran <assasin2325@mail.ru>
2026-09-09 20:27:51 +0400

    обновил HTTTP на HTTPS

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // website/js/config.js
 // Пока тестируем локально. Позже заменишь на реальный адрес бэкенда.
-export const API_BASE_URL = "http://194.226.122.252:8000/api/v1";
+export const API_BASE_URL = "https://194.226.122.252:8443/api/v1";
